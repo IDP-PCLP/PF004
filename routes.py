0bbb37f789156b3c66e130d2c83bdde52fed950e
Álvaro Cafe <alvaro.campos.ferreira@gmail.com>
2021-05-17 20:26:32 -0300

    Adicionando arquivos iniciais.

--- routes.py
+++ routes.py
@@ -1,4 +1,5 @@
-# CIDADÃO DF
+"""
+CIDADÃO DF
 O aplicativo “Cidadão DF”, possibilita o registro de um problema em sua exata localização, dimensão e horário; e deixa o cidadão no anonimato. Registra qualquer tipo de problema com uma foto, que inclui automaticamente sua localização e momento em que foi tirada, comprovando e expondo os problemas. Desde um buraco na rua até a presença de grupos suspeitos, o aplicativo tem por objetivo otimizar o trabalho dos órgãos competentes, na identificação dos problemas de infraestrutura e segurança que assolam um número considerável de cidadãos, possibilitando uma rápida solução na atuação dos problemas.
 
 Objetivos:
@@ -11,5 +12,17 @@ Não necessitando realizar um cadastro, o App permite categorizar a denúncia:
 - SEGURANÇA: referente a suspeitas de tráfico, vadiagem, venda ilegal de armas, lavagem de dinheiro, contrabando, pontos de distribuição de drogas e outros problemas relacionados à Segurança,
 - TRANSPORTE: referente a lotações, falta de ônibus, precariedades no transporte público e outros problemas relacionados ao Transporte Público;
 - VIOLÊNCIA: referente a denúncias de violência doméstica, e outras.
+"""
 
-Projeto proposto por um dos estudantes da disciplina no primeiro semestre de 2021.
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+        return 'Hello, World!'
+
+
+if __name__ == '__main__':
+    app.run()
