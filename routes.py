873dce08a183380931666b3cdf0eb463584c431b
Álvaro Cafe <alvaro.campos.ferreira@gmail.com>
2021-05-31 20:31:40 -0300

    Adicionando funcionalidade de enviar arquivo no template violencia.

--- routes.py
+++ routes.py
@@ -17,13 +17,24 @@ Não necessitando realizar um cadastro, o App permite categorizar a denúncia:
 """
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, SelectField # Os validadores são usados para garantir que as entradas sejam o que a gente espera.
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, SelectField# Os validadores são usados para garantir que as entradas sejam o que a gente espera
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 from flask import render_template, Blueprint, redirect, url_for, flash, request
 from flask import Flask, render_template, abort
+import secrets, os
+from PIL import Image
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'minhasenha'
+
+class EnviarForm(FlaskForm):
+    picture = FileField('Escolher foto', validators=[FileAllowed(['jpg', 'jpeg', 'png'])])
+    comentario = StringField('Comentários')
+    submit = SubmitField('Enviar')
+
+
 
 @app.route('/')
 def index(): 
@@ -53,9 +64,31 @@ def transporte():
 def viasPublicas(): 
     return render_template('viasPublicas.html')
 
-@app.route('/violencia')
+@app.route('/violencia', methods=['GET','POST'])
 def violencia(): 
-    return render_template('violencia.html')
+    form = EnviarForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+        return render_template('index.html')
+
+    return render_template('violencia.html',form=form)
+
+# Essa função vai ser usada para salvar a imagem
+def save_picture(form_picture):
+   # Para salvar a imagem, é interessante mudar o nome para não conflitar com alguma já existente. Vamos utilizar o módulo secrets.
+    random_hex = secrets.token_hex(8)
+    f_name, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/images', picture_fn)
+
+    # Alterando tamanho e salvando
+    output_size = (250, 250)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    
+    return picture_fn
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5002, debug=True)
